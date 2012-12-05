0d416ecec50b0c789d47fffe0feecc6589b74a09
Oleg Mirzov <oleg.mirzov@gmail.com>
2012-12-05 22:53:49 +0100

    Build definition updates

--- project/Build.scala
+++ project/Build.scala
@@ -6,10 +6,11 @@ object ApplicationBuild extends Build {
 
     val appName         = "gameoflife"
     val appVersion      = "1.0"
+    val scalaVersion    = "2.10.0-RC3"
 
     val appDependencies = Seq(
-	    "joda-time" % "joda-time" % "2.1",
-			"org.scala-lang" % "scala-actors" % "2.10.0-RC2"
+	"joda-time" % "joda-time" % "2.1",
+	"org.scala-lang" % "scala-actors" % "2.10.0-RC3"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
